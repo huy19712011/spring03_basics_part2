7d13c781b797ec96737ff9c013022222f3b7208e
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-09-14 09:23:29 +0700

    initial commit

---
+++ src/main/java/com/mycompany/spring03_basics_part2/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.spring03_basics_part2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
